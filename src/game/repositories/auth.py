8273497f19fbb1a79fac6e16b328bfcf5e9454e5
Fedor Obolihin <fedor.obolihin@zyfra.com>
2023-06-18 14:18:50 +0300

    fix rules

--- src/game/repositories/auth.py
+++ src/game/repositories/auth.py
@@ -9,12 +9,12 @@ class AuthRepository(BaseRepository):
     AUTH_TABLE = settings.AUTH_TABLE
 
     def select_one(self, token: str, user_id: int) -> str:
-        jwt_token = self._cursor.execute(
-            f"SELECT jwt FROM {self.AUTH_TABLE} WHERE user_id = {user_id} AND jwt = '{token}'",
-        ).fetchone()
+        query = f"SELECT jwt FROM {self.AUTH_TABLE} WHERE user_id = {user_id} AND jwt = '{token}'"
+        jwt_token = self._cursor.execute(query).fetchone()
         if jwt_token is None:
             raise AuthenticationException("Не верный токен")
         return jwt_token[0]
 
     def insert(self, token: str, user_id: int) -> None:
-        self._cursor.execute(f"INSERT INTO {self.AUTH_TABLE} (user_id, jwt) VALUES ({user_id}, '{token}')")
+        query = f"INSERT INTO {self.AUTH_TABLE} (user_id, jwt) VALUES ({user_id}, '{token}')"
+        self._cursor.execute(query)
